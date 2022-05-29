dd7dea11cf5d19ede26a5fcb6a33a723cb1dabb0
llf <1010384066@qq.com>
2022-05-29 18:15:35 +0800

    添加play按钮

--- adding_play_button/alien_invasion.py
+++ adding_play_button/alien_invasion.py
@@ -1,1 +1,15 @@
-import sys
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption("Alien Invasion")
---
+++ adding_play_button/game_stats.py
@@ -0,0 +1,7 @@
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
