05323981be6de9e9e7f43198fa473d591489e537
llf <1010384066@qq.com>
2022-05-29 18:37:09 +0800

    button类，绘制按钮

--- adding_play_button/alien_invasion.py
+++ adding_play_button/alien_invasion.py
@@ -7,7 +7,6 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.settings = Settings()
 
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
